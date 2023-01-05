3bc0740a96f2a51ef58630c6206dbb0b037691ea
Thomas Reber <treber@live.com>
2023-01-05 17:03:23 +0100

    initial commit

---
+++ startup.m
@@ -0,0 +1,5 @@
+% add common-analyses-scripts to the path (git@github.com:mormannlab/common_analysis_scripts.git), 
+
+baseDirToScripts = '~/scripts'; % adjust this depending on your setup, I assume common analysis scripts are checked out to a subdirectory of 'scripts' in a mac/linux home folder
+addpath(genpath([baseDirToScripts, filesep, 'common_analysis_scripts']));
+
