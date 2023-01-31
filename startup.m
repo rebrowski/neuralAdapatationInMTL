db727b5ab8d0ab53beffceb2942b07111d3b8f02
Thomas Reber <treber@dorper>
2023-01-31 17:07:06 +0100

    changed some paths for it to also work on our linux workstation

--- startup.m
+++ startup.m
@@ -1,7 +1,12 @@
 %% adjust this to your situation>
-
 addpath(genpath('~/scripts/common_analysis_scripts')); %path to common_analysis_scripts: https://github.com/mormannlab/common_analysis_scriptsN
 addpath('~/scripts/neuralAdapatationInMTL'); % scripts in this repo: https://github.com/rebrowski/neuralAdapatationInMTL
 stimdir = '~/scripts/neuralAdapatationInMTL/stimuli/'; % folder with images contained in this repo
-datadir = '~/projects/ospr/secondlevel/'; %path to iEEG (ieeg_linked_mastoids_256Hz.mat) and single unit data (sessions.mat)
+
+% path to large datafiles that need to be downloaded separately 
+% like the iEEG traces (ieeg_linked_mastoids_256Hz.mat) and single unit data (sessions.mat)
+datadir = '~/projects/ospr/secondlevel/'; 
+if isunix
+    datadir = '~/media/treber/neuralAdapatationInMTL/';
+end
 
